e75260d26d36b2858611c241ea224103ec3c8967
Mohamamd Hammad <hammad.scripted@gmail.com>
2026-05-01 18:30:21 +0530

    implement note fetching logic in NoteDetailPage

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -1,13 +1,35 @@
-import { useState } from 'react';
-import useNavigate, { useParams } from 'react-router';
+import axiosInstance from '../libs/axios.js';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import toast from 'react-hot-toast';
 const NoteDetailPage = () => {
   const [note, setNote] = useState(null);
   const [loading, setLoading] = useState(true);
   const [saving, setSaving] = useState(false);
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const { id } = useParams();
-  return <div>Note</div>;
+
+  useEffect(() => {
+    const fetchNote = async () => {
+      try {
+        const res = await axiosInstance.get(`/notes/${id}`);
+        setNote(res.data);
+        console.log(res.data);
+      } catch (error) {
+        toast.error('Failed to fetch note');
+        console.log('error', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchNote();
+  }, [id]);
+  return (
+    <div>
+      <h1>Details of note</h1>
+    </div>
+  );
 };
 
 export default NoteDetailPage;
