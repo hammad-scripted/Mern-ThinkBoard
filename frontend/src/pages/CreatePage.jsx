e7a9034e4127d475d75d04791fd82aaf1fde508a
Mohamamd Hammad <hammad.scripted@gmail.com>
2026-05-02 16:52:42 +0530

    axios bug fix

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -2,7 +2,7 @@ import { ArrowLeftIcon } from 'lucide-react';
 import { useState } from 'react';
 import toast from 'react-hot-toast';
 import { Link, useNavigate } from 'react-router';
-import axios from 'axios';
+import axiosInstance from '../libs/axios';
 const CreatePage = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
@@ -19,7 +19,7 @@ const CreatePage = () => {
     }
     setLoading(true);
     try {
-      await axios.post('http://localhost:5001/api/notes/', { title, content });
+      await axiosInstance.post('/notes', { title, content });
       toast.success('Note created successfully');
       navigate('/');
     } catch (error) {
