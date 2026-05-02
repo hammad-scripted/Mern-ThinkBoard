8befb48396bb0e45d3abaf8a4309086b3237668e
Mohamamd Hammad <hammad.scripted@gmail.com>
2026-05-02 16:30:29 +0530

    doing production setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,25 +5,37 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 import notesRouter from './routes/notesRoutes.js';
+import path from 'node:path';
 import { connectDb } from './config/db.js';
 import chalk from 'chalk';
 import { rateLimiter } from './middlewares/rateLimiter.js';
 const PORT = process.env.PORT || 5001;
 const app = express();
+const __dirname = path.resolve();
 
 // ** MIDDLEWARE **
 // // rate limiter
-app.use(
-  cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  }),
-);
+
+if (process.env.NODE_ENV !== 'production') {
+  app.use(
+    cors({
+      origin: 'http://localhost:5173',
+    }),
+  );
+}
 app.use(express.json());
 app.use(rateLimiter);
 
 // ** ROUTES **
 app.use('/api/notes', notesRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  });
+}
 
 connectDb()
   .then((connection) => {
